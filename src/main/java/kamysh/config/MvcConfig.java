b9131b32c9fbab10271a72c66e6a777a8252a9fc
LittlePyatochka <lexica2000@mail.ru>
2022-03-19 15:52:09 +0300

    fix

--- src/main/java/kamysh/config/MvcConfig.java
+++ src/main/java/kamysh/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package kamysh.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
